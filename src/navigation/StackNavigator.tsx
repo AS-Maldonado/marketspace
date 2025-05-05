7bed608fe8268fa94ecf6fde0b9e8e4347e1546e
Augusto <asmaldonado02@gmail.com>
2025-05-05 06:57:48 -0300

    first commit

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/login";
+import RegisterSceen from "../screens/register";
+
+type RootStackParams = {
+  Login: undefined;
+  Register: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
+
+export default function StackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterSceen} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/register.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function RegisterSceen() {
+  return <Text>Register</Text>;
+}
